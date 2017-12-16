5ead8a084c18064fb9d47a0eee81a60b6a8a93a3
AlexKhamitov <s-hamtov@mail.ru>
2017-12-17 01:13:13 +0300

    handle exeptions

--- main.cpp
+++ main.cpp
@@ -4,30 +4,38 @@
 #include <cmath>
 #include <experimental/filesystem>
 #include "libaa.hpp"
+#include <string>
 
 using namespace Magick;
 using namespace aalib;
 using std::size_t;
 using namespace std::experimental::filesystem;
 
-void print_text_to_file(unsigned char* text, Context& context)
+void print_text_to_file(unsigned char* text, Context& context, std::string directory)
 {
-    std::ofstream text_stream("../IMG_to_ASCII/sources/ascii.html");
-    text_stream.exceptions(std::ios_base::failbit|std::ios_base::badbit);
-    text_stream << "<html><br />"
-                   "<body bgcolor=\"#000000\" text=\"#b2b2b2\"<br />"
-                   "<font color =#b2b2b2 size=2><pre>";
-    for(size_t y = 0; y < context.get_scr_height(); ++y)
+    try
     {
-        for(size_t x = 0; x < context.get_scr_width(); ++x)
+        std::ofstream text_stream(directory + std::string("ascii.html"));
+        text_stream.exceptions(std::ios_base::failbit|std::ios_base::badbit);
+        text_stream << "<html><br />"
+                       "<body bgcolor=\"#000000\" text=\"#b2b2b2\"<br />"
+                       "<font color =#b2b2b2 size=2><pre>";
+        for(size_t y = 0; y < context.get_scr_height(); ++y)
         {
-            text_stream << text[y*context.get_scr_width() + x];
+            for(size_t x = 0; x < context.get_scr_width(); ++x)
+            {
+                text_stream << text[y*context.get_scr_width() + x];
+            }
+            text_stream << "<br />";
         }
-        text_stream << "<br />";
-    }
 
-    text_stream << "</pre></font></body><br />"
-                   "</html>";
+        text_stream << "</pre></font></body><br />"
+                       "</html>";
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
 
 int pixel_to_aacolor(Color& color)
@@ -41,20 +49,36 @@ int pixel_to_aacolor(Color& color)
 }
 
 int main(int args, char * argv[]) {
-    Image image("../IMG_to_ASCII/sources/a.png");
-    Context context(image.columns(), image.rows());
-    context.resize();
-
-    for(size_t x = 0; x < context.get_img_width(); ++x)
+    //Image image("../IMG_to_ASCII/sources/a.png");
+    if(args < 3)
     {
-        for(size_t y = 0; y < context.get_img_height(); ++y)
-        {
-            Color color = image.pixelColor(x,y);
-            context.put_pixel(x, y,pixel_to_aacolor(color));
-        }
+        std::cout << "Write directory and image name" << '\n';
+        return 0;
     }
+    try
+    {
+        std::string directory(argv[1]);
+        std::string image_name(argv[2]);
+        Image image(directory + image_name);
+        Context context(image.columns(), image.rows());
+        context.resize();
 
-    context.render();
-    unsigned char *text = context.get_text();
-    print_text_to_file(text, context);
+        for(size_t x = 0; x < context.get_img_width(); ++x)
+        {
+            for(size_t y = 0; y < context.get_img_height(); ++y)
+            {
+                Color color = image.pixelColor(x,y);
+                context.put_pixel(x, y,pixel_to_aacolor(color));
+            }
+        }
+
+        context.render();
+        unsigned char *text = context.get_text();
+        print_text_to_file(text, context, directory);
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
 }
